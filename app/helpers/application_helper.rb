84b59bfa70281e2fb727eb33cf323eff1862972e
Damien Tailhades <hello@regulardesigner.com>
2015-10-06 11:38:19 +0200

    layout and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,26 @@
 module ApplicationHelper
+	def title(page_title)
+    	content_for(:title) { page_title }
+	end
+  
+	def metadesc(page_metadesc)
+    	content_for(:metadesc) { page_metadesc }
+	end
+
 	def markdown(text)
 		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
 		no_intra_emphasis: true, 
     	fenced_code_blocks: true,   
     	disable_indented_code_blocks: true)
 	return markdown.render(text).html_safe
+	end
+
+	def draftCounter
+		@count = Story.where(status: "Draft").count
+		if @count >= 1
+			puts = "&#9998; Your draft <span class='label round secondary'>#{@count}</span>".html_safe
+		else
+			puts = "&#9998; Your draft".html_safe
+		end
 	end
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,11 @@
 module StoriesHelper
+	def message
+		@count = Story.where(status: "Publish").count
+		if @count > 1
+			@pluralize = "s"
+		else
+			@pluralize = ""
+		end
+		puts = "Vous avez #{@count} message#{@pluralize} publié#{@pluralize}"
+	end
 end
